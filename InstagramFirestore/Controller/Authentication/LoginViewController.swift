5209067b2e5c658f532108fdc1bc6e05f887c370
Junior Silva <nj.development83@gmail.com>
2021-10-03 18:09:57 -0300

    Created and implemented LoginVC, created CustomTF and UIButton extension

---
+++ InstagramFirestore/Controller/Authentication/LoginViewController.swift
@@ -0,0 +1,96 @@
+//
+//  LoginViewController.swift
+//  InstagramFirestore
+//
+//  Created by Junior Silva on 03/10/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let iconImage: UIImageView = {
+        let image = UIImageView(image: #imageLiteral(resourceName: "Instagram_logo_white"))
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
+    
+    private let emailTextField: UITextField = {
+        let textField = CustomTextField(placeholder: "E-mail")
+        textField.keyboardType = .emailAddress
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = CustomTextField(placeholder: "Password")
+        textField.keyboardType = .emailAddress
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log in", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(didTapLoginButton), for: .touchUpInside)
+        button.setHeight(50)
+        return button
+    }()
+    
+    private let forgotPasswordButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.attributedTitle(firstPart: "Forgot your password?", secondPart: "Get help signing in.")
+        return button
+    }()
+    
+    private let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.attributedTitle(firstPart: "Don't have an account?", secondPart: "Sign Up")
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .white
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+        
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemBlue.cgColor]
+        gradient.locations = [0, 1]
+        view.layer.addSublayer(gradient)
+        gradient.frame = view.frame
+        
+        view.addSubview(iconImage)
+        iconImage.centerX(inView: view)
+        iconImage.setDimensions(height: 80, width: 120)
+        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton, forgotPasswordButton])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 20
+        
+        view.addSubview(stackView)
+        stackView.anchor(top: iconImage.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.centerX(inView: view)
+        dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor)
+    }
+    
+    @objc func didTapLoginButton() {
+        print("Tapped")
+    }
+    
+}
---
+++ InstagramFirestore/Controller/Authentication/RegistrationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationViewController.swift
+//  InstagramFirestore
+//
+//  Created by Junior Silva on 03/10/21.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}

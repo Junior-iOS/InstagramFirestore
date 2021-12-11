ce772b9347331f75ad4c60beb50125d48a932635
Nelson Junior <nsilva.solks@digio.com.br>
2021-12-11 00:24:25 -0300

    Ordering posts and notifications by timestamp

--- InstagramFirestore/API/PostService.swift
+++ InstagramFirestore/API/PostService.swift
@@ -51,10 +51,7 @@ struct PostService {
         query.getDocuments { snapshot, error in
             guard let documents = snapshot?.documents else { return }
             var posts = documents.map({ Post(postId: $0.documentID, dictionary: $0.data()) })
-            
-            posts.sort { (post1, post2) -> Bool in
-                return post1.timestamp.seconds > post2.timestamp.seconds
-            }
+            posts.sort(by: { $0.timestamp.seconds > $1.timestamp.seconds })
             
             completion(posts)
         }
@@ -98,6 +95,8 @@ struct PostService {
             snapshot?.documents.forEach({ document in
                 fetchPost(withPostId: document.documentID) { post in
                     posts.append(post)
+                    posts.sort(by: { $0.timestamp.seconds > $1.timestamp.seconds })
+                    
                     completion(posts)
                 }
             })
